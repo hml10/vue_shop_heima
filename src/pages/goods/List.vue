f95cc7b4527a0831fe2c527a06cb653acdacc68b
hml <2665076656@qq.com>
2022-03-28 01:27:40 +0800

    商品列表

--- src/pages/goods/List.vue
+++ src/pages/goods/List.vue
@@ -10,9 +10,18 @@
     <!-- 卡片视图区域 -->
     <el-card>
       <el-row :gutter="20">
-        <el-col :span="8">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+        <el-col :span="12">
+          <el-input
+            clearable
+            @clear="getGoodsList"
+            placeholder="请输入搜索内容"
+            v-model="queryInfo.query"
+          >
+            <el-button
+              slot="append"
+              icon="el-icon-search"
+              @click="getGoodsList"
+            ></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
@@ -27,21 +36,21 @@
         <el-table-column
           label="商品价格 / 元"
           prop="goods_price"
-          width="100px"
+          width="95px"
         ></el-table-column>
         <el-table-column
           label="商品重量"
           prop="goods_weight"
-          width="100px"
+          width="75px"
         ></el-table-column>
-        <el-table-column label="创建时间" prop="add_time" width="140px">
+        <el-table-column label="创建时间" prop="add_time" width="130px">
           <template slot-scope="scope">
             <!-- {{ scope.add_time | changeTime }} -->
             {{ scope.add_time | formonment('YYYY年-MM月-DD日 HH时:mm分:ss秒') }}
           </template>
         </el-table-column>
         <el-table-column label="操作" width="120px">
-          <template>
+          <template slot-scope="scope">
             <el-button
               type="primary"
               icon="el-icon-edit"
@@ -51,10 +60,24 @@
               type="danger"
               icon="el-icon-delete"
               size="mini"
+              @click="removeById(scope.row.goods_id)"
             ></el-button>
           </template>
         </el-table-column>
       </el-table>
+
+      <!-- 分页区域 -->
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15, 20]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -88,9 +111,38 @@ export default {
       if (res.meta.status !== 200) {
         return this.$message.error('获取商品列表数据失败！');
       }
-      console.log('商品列表', res.data.goods);
+      // console.log('##', res.data.goods);
       this.total = res.data.total;
       this.goodsList = res.data.goods;
+    },
+
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize;
+      this.getGoodsList();
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage;
+      this.getGoodsList();
+    },
+
+    // 根据id删除
+    removeById(id) {
+      this.$confirm('此操作将永久删除, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      })
+        .then(async () => {
+          const { data: res } = await this.$http.delete('goods/' + id);
+          if (res.meta.status !== 200) {
+            return this.$message.error('删除失败！');
+          }
+          this.$message.success('删除成功！');
+          this.getGoodsList();
+        })
+        .catch(() => {
+          this.$message.info('已取消删除！');
+        });
     },
   },
 };
